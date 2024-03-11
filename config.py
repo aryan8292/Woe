f33a7d4cba14cb5b861c99746c917419d6644760
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-03-12 00:42:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = "3334521"  # api id
+API_HASH = "29edd7420d528140c7a04bd47486886f"  # api hash
 
 BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
 
